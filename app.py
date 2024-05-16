22b4c4cb5a44e016d42a8729417400f196688e80
ShiningSolar <144994505+ShiningSolar@users.noreply.github.com>
2024-05-17 01:55:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,27 +2,30 @@ import pickle
 import streamlit as st
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.neighbors import NearestNeighbors
 
 st.header("Recipe Recommendation System")
+model = pickle.load(open('artifacts/model.pkl', 'rb'))
+tfidf = pickle.load(open('artifacts/tfidf.pkl', 'rb'))
 dataset = pickle.load(open('dataset.pkl', 'rb'))
+image = pickle.load(open('image.pkl', 'rb'))
+ingredient = pickle.load(open('ingredient.pkl', 'rb'))
+info = pickle.load(open('info.pkl', 'rb'))
+recipeName = pickle.load(open('recipeName.pkl', 'rb'))
 
-# Function to calculate cosine similarity between two vectors
-def calculate_cosine_similarity(embedding1, embedding2):
-    if embedding1 is None or embedding2 is None:
-        return None
-    return cosine_similarity([embedding1], [embedding2])[0][0]
+# Function to generate recommendations based on knn
+def generate_knn_recommendations(item_id, df, knn_model, n_neighbors=10):
+    #item_index = df[df['item_id'] == item_id].index[0]
+    item_index = item_id
+    distances, indices = knn_model.kneighbors(tfidf[item_index], n_neighbors=n_neighbors + 1)
+    similar_items = df.iloc[indices[0][1:]]  # Menghapus item itu sendiri dari hasil
+    return similar_items
 
-# Function to generate recommendations based on cosine similarity
-def generate_recommendations(query_embedding, df, top_n=5):
-    similarities = df['embedding'].apply(lambda x: calculate_cosine_similarity(query_embedding, x))
-    df['similarity'] = similarities
-    recommendations = df.sort_values(by='similarity', ascending=False).head(top_n)
-    return recommendations.drop(columns=['embedding', 'similarity', 'RecipeId'])
 
-# Example usage: generate recommendations based on the first row embedding
-query_embedding = dataset['embedding'][1]
-recommendations = generate_recommendations(query_embedding, dataset)
-#print("Recommendations based on the first row embedding:")
+
+# Contoh penggunaan: merekomendasikan item berdasarkan item_id 1
+recommendations = generate_knn_recommendations(96, info, model)
+print("Recommendations based on item_id 1:")
 
 df = recommendations
 # Display the DataFrame without index
