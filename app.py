7c9dbb0f6bd9b4396460738b049beb87f486ed6c
ShiningSolar <144994505+ShiningSolar@users.noreply.github.com>
2024-05-17 16:03:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,20 +14,30 @@ info = pickle.load(open('info.pkl', 'rb'))
 recipeName = pickle.load(open('recipeName.pkl', 'rb'))
 
 # Function to generate recommendations based on knn
-def generate_knn_recommendations(item_id, df, knn_model, n_neighbors=10):
+def generate_knn_recommendations(recipeName, df, knn_model, n_neighbors=10):
     #item_index = df[df['item_id'] == item_id].index[0]
-    item_index = item_id
+    item_index = np.where(info.index == recipeName)[0][0]
     distances, indices = knn_model.kneighbors(tfidf[item_index], n_neighbors=n_neighbors + 1)
     similar_items = df.iloc[indices[0][1:]]  # Menghapus item itu sendiri dari hasil
     return similar_items
 
+selected_recipe = st.selectbox(
+    "Type or select a recipe",
+    recipeName
+)
+
+if st.button('Show Recommendation'):
+    recommendations = generate_knn_recommendations(recipeName, info, model)
+    st.dataframe(recommendations, hide_index = True)
 
 
 # Contoh penggunaan: merekomendasikan item berdasarkan item_id 1
-recommendations = generate_knn_recommendations(96, info, model)
-print("Recommendations based on item_id 1:")
+#recommendations = generate_knn_recommendations(96, info, model)
+#print("Recommendations based on item_id 1:")
 
-df = recommendations
+#df = recommendations
 # Display the DataFrame without index
 #df = df.style.hide_index()
-st.dataframe(df, hide_index = True)
+
+
+
