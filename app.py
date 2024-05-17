3f7692f6ab43b6acb6b7edf3e68fa7ead1f2cc2b
ShiningSolar <144994505+ShiningSolar@users.noreply.github.com>
2024-05-17 16:38:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ recipeName = pickle.load(open('recipeName.pkl', 'rb'))
 # Function to generate recommendations based on knn
 def generate_knn_recommendations(name, df, knn_model, n_neighbors=10):
     #item_index = df[df['item_id'] == item_id].index[0]
-    item_index = name
+    item_index = df[df['Name'] == name].index[0]
     distances, indices = knn_model.kneighbors(tfidf[item_index], n_neighbors=n_neighbors + 1)
     similar_items = df.iloc[indices[0][1:]]  # Menghapus item itu sendiri dari hasil
     return similar_items
@@ -29,8 +29,8 @@ selected_recipe = st.selectbox(
 
 if st.button('Show Recommendation'):
     st.write(selected_recipe)
-#    recommendations = generate_knn_recommendations(selected_recipe, info, model)
-#    st.dataframe(recommendations, hide_index = True)
+    recommendations = generate_knn_recommendations(selected_recipe, info, model)
+    st.dataframe(recommendations, hide_index = True)
 
 
 # Contoh penggunaan: merekomendasikan item berdasarkan item_id 1
