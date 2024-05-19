01caae1b9cbeca79e9b94c6f6b6b299856386777
ShiningSolar <144994505+ShiningSolar@users.noreply.github.com>
2024-05-19 10:55:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def fragment_function():
                 tile = tile.columns(2)
                 tile[0] = tile[0].image(recipe_image[index])
                 #tile[1] = tile[1].link_button(recipe_name[index], "https://recipe-recommendation-system-with-content-based-filtering-1008.streamlit.app/recipe_page")
-                tile[1] = tile[1].page_link("pages/recipe_page.py", label=recipe_name[index])
+                tile[1] = tile[1].page_link("pages/recipe_page.py", label=recipe_name[index], use_container_width = True)
                 index = index + 1
     if selected_recipe == '':
         recommendation_box.empty()
