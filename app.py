42bce2d39c7b90d8cfdc6b68544d370a033fa433
ShiningSolar <144994505+ShiningSolar@users.noreply.github.com>
2024-05-19 11:25:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -77,8 +77,9 @@ def fragment_function():
                 #tile[1] = tile[1].page_link("pages/recipe_page.py", label=recipe_name[index], use_container_width = True)
                 #page_button = tile[1].empty()
                 name = recipe_name[index]
-                if tile[1].button(label = name):
-                    recipe_details(name)
+                if "recipe_details" not in st.session_state:
+                    if tile[1].button(label = name):
+                        recipe_details(name)
                 index = index + 1
     if selected_recipe == '':
         recommendation_box.empty()
